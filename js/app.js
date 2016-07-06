836ec67a39f394425fc6f08a1a43b0771ac32f57
nikthequik <quik.the.nick@gmail.com>
2016-07-05 18:53:58 -0600

    -add partials -adjust css positioning for new wireframe

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
 (function(){
-	angular.module("tipCalc", []);
+	angular.module("tipCalc", ['ngRoute'])
+	.config(['$routeProvider', function($routeProvider){
+		$routeProvider.when('/', {
+			templateUrl: 'home.html',
+			controller: 'mealCtrl as vm'
+		})
+		.when('/home', {
+			templateUrl: 'home.html',
+			controller: 'mealCtrl as vm'
+		})
+		.when('/new-meal', {
+			templateUrl: 'new-meal.html',
+			controller: 'mealCtrl as vm'
+		})
+		.when('/my-earnings', {
+			templateUrl: 'my-earnings.html',
+			controller: 'mealCtrl as vm'
+		})
+		.when('/error', {
+			templateUrl: 'error.html',
+			controller: 'mealCtrl as vm'
+		})
+		.otherwise('/error')
+	}]);
 })();
 
